cfc42442aafc2d84e17c33e2e9eff014aa27dfec
Rafael Bodill <justrafi@gmail.com>
2015-03-22 01:25:19 +0200

    Adding Karabiner options and settings

---
+++ karabiner/README.md
@@ -0,0 +1,25 @@
+Karabiner
+===
+A powerful and stable keyboard customizer for OS X.
+
+Installation
+---
+See https://pqrs.org/osx/karabiner/
+
+### Custom Options
+```sh
+cd "~/Library/Application Support/Karabiner"
+ln -sfv ~/.config/karabiner/private.xml
+/Applications/Karabiner.app/Contents/Library/bin/karabiner reloadxml
+```
+
+Import Settings
+---
+```sh
+sh ~/.config/karabiner/import.sh
+```
+
+See Also
+---
+- Documentation: https://pqrs.org/osx/karabiner/document.html.en
+- Custom options: https://pqrs.org/osx/karabiner/document.html.en#privatexml
---
+++ karabiner/import.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set remap.uk_saneuklayout2 1
+/bin/echo -n .
+$cli set remap.optionR2controlR 1
+/bin/echo -n .
+$cli set repeat.initial_wait 400
+/bin/echo -n .
+$cli set remap.shiftL2controlL 1
+/bin/echo -n .
+$cli set remap.move_mouse_cursor_center_fn_alone 1
+/bin/echo -n .
+$cli set remap.consumer_to_cmd_fkeys_f7 1
+/bin/echo -n .
+$cli set repeat.wait 70
+/bin/echo -n .
+$cli set private.feighttosixteen 1
+/bin/echo -n .
+$cli set remap.fn_scroll_to_brightness_control 1
+/bin/echo -n .
+$cli set parameter.keyoverlaidmodifier_timeout 300
+/bin/echo -n .
+$cli set remap.commandR2optionR 1
+/bin/echo -n .
+$cli set remap.shiftR2shiftR_escape 1
+/bin/echo -n .
+/bin/echo
