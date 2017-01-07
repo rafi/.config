3d2489d652e432bc0de2a2e11c9c6a13e69c837d
Rafael Bodill <justrafi@gmail.com>
2017-01-07 16:27:48 +0200

    Update ranger configuration

--- ranger/scope.sh
+++ ranger/scope.sh
@@ -16,25 +16,27 @@
 # 3    | fix width  | success. Don't reload when width changes
 # 4    | fix height | success. Don't reload when height changes
 # 5    | fix both   | success. Don't ever reload
+# 6    | image      | success. display the image $cached points to as an image preview
 
 # Meaningful aliases for arguments:
 path="$1"    # Full path of the selected file
 width="$2"   # Width of the preview pane (number of fitting characters)
 height="$3"  # Height of the preview pane (number of fitting characters)
+cached="$4"  # Path that should be used to cache image previews
 
 maxln=200    # Stop after $maxln lines.  Can be used like ls | head -n $maxln
 
 # Find out something about the file:
 mimetype=$(file --mime-type -Lb "$path")
-extension=${path##*.}
+extension=$(/bin/echo "${path##*.}" | tr "[:upper:]" "[:lower:]")
 
 # Functions:
 # runs a command and saves its output into $output.  Useful if you need
 # the return value AND want to use the output in a pipe
 try() { output=$(eval '"$@"'); }
 
-# writes the output of the previouosly used "try" command
-dump() { echo "$output"; }
+# writes the output of the previously used "try" command
+dump() { /bin/echo "$output"; }
 
 # a common post-processing function used after most commands
 trim() { head -n "$maxln"; }
@@ -65,6 +67,7 @@ case "$extension" in
         try lynx   -dump "$path" && { dump | trim | fmt -s -w $width; exit 4; }
         try elinks -dump "$path" && { dump | trim | fmt -s -w $width; exit 4; }
         highlight --out-format=ansi --syntax=html "$path" && exit 5 || exit 1;;
+#        ;; # fall back to highlight/cat if the text browsers fail
     # Bash dotfiles:
     bash*)
         highlight --out-format=ansi --syntax=bash "$path" && exit 5 || exit 2;;
@@ -83,14 +86,21 @@ case "$mimetype" in
     # Ascii-previews of images:
     image/*)
         try cam -W "$width" "$path" && { dump | trim; exit 5; }
-        try img2txt --gamma=0.6 --width="$width" "$path" && { dump | trim; exit 5; } || exit 1;;
-#    # Display information about media files:
+        img2txt --gamma=0.6 --width="$width" "$path" && exit 4 || exit 1;;
+    # Image preview for videos, disabled by default:
+    # video/*)
+    #     ffmpegthumbnailer -i "$path" -o "$cached" -s 0 && exit 6 || exit 1;;
+    # Display information about media files:
     video/* | audio/*)
         try mediainfo "$path" && { dump | trim | sed 's/  \+:/: /;';  exit 5; }
         try id3info "$path" && { dump | trim; exit 5; }
         try id3ted -L "$path" && { dump | trim | sed 's/(User defined.\+): //;'; exit 5; }
         try exiftool -v "$path" && { dump | trim; exit 5; }
-        try exiftool-5.16 -v "$path" && { dump | trim; exit 5; } || exit 1;;
+#        try exiftool-5.16 -v "$path" && { dump | trim; exit 5; } || exit 1;;
+#        exiftool "$path" && exit 5
+        # Use sed to remove spaces so the output fits into the narrow window
+#        try mediainfo "$path" && { dump | trim | sed 's/  \+:/: /;';  exit 5; } || exit 1;;
+
 esac
 
 exit 1
