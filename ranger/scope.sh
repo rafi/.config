92b1243613b048963afaf564da5ff2fb34b6fd05
Rafael Bodill <justrafi@gmail.com>
2017-05-09 13:06:59 +0300

    Update ranger configuration

--- ranger/scope.sh
+++ ranger/scope.sh
@@ -17,18 +17,20 @@
 # 4    | fix height | success. Don't reload when height changes
 # 5    | fix both   | success. Don't ever reload
 # 6    | image      | success. display the image $cached points to as an image preview
+# 7    | image      | success. display the file directly as an image
 
 # Meaningful aliases for arguments:
-path="$1"    # Full path of the selected file
-width="$2"   # Width of the preview pane (number of fitting characters)
-height="$3"  # Height of the preview pane (number of fitting characters)
-cached="$4"  # Path that should be used to cache image previews
+path="$1"            # Full path of the selected file
+width="$2"           # Width of the preview pane (number of fitting characters)
+height="$3"          # Height of the preview pane (number of fitting characters)
+cached="$4"          # Path that should be used to cache image previews
+preview_images="$5"  # "True" if image previews are enabled, "False" otherwise.
 
 maxln=200    # Stop after $maxln lines.  Can be used like ls | head -n $maxln
 
 # Find out something about the file:
 mimetype=$(file --mime-type -Lb "$path")
-extension=$(/bin/echo "${path##*.}" | tr "[:upper:]" "[:lower:]")
+extension=$(/bin/echo "${path##*.}" | awk '{print tolower($0)}')
 
 # Functions:
 # runs a command and saves its output into $output.  Useful if you need
@@ -42,65 +44,94 @@ dump() { /bin/echo "$output"; }
 trim() { head -n "$maxln"; }
 
 # wraps highlight to treat exit code 141 (killed by SIGPIPE) as success
-highlight() { command highlight "$@"; test $? = 0 -o $? = 141; }
+safepipe() { "$@"; test $? = 0 -o $? = 141; }
+
+# Image previews, if enabled in ranger.
+if [ "$preview_images" = "True" ]; then
+    case "$mimetype" in
+        # Image previews for SVG files, disabled by default.
+        ###image/svg+xml)
+        ###   convert "$path" "$cached" && exit 6 || exit 1;;
+        # Image previews for image files. w3mimgdisplay will be called for all
+        # image files (unless overriden as above), but might fail for
+        # unsupported types.
+        image/*)
+            exit 7;;
+        # Image preview for video, disabled by default.:
+        ###video/*)
+        ###    ffmpegthumbnailer -i "$path" -o "$cached" -s 0 && exit 6 || exit 1;;
+    esac
+fi
 
 case "$extension" in
     # Archive extensions:
-    7z|a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|\
+    a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|\
     rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)
         try als "$path" && { dump | trim; exit 0; }
         try acat "$path" && { dump | trim; exit 3; }
         try bsdtar -lf "$path" && { dump | trim; exit 0; }
         exit 1;;
     rar)
+        # avoid password prompt by providing empty password
         try unrar -p- lt "$path" && { dump | trim; exit 0; } || exit 1;;
+    7z)
+        # avoid password prompt by providing empty password
+        try 7z -p l "$path" && { dump | trim; exit 0; } || exit 1;;
     # PDF documents:
     pdf)
         try pdftotext -l 10 -nopgbrk -q "$path" - && \
             { dump | trim | fmt -s -w $width; exit 0; } || exit 1;;
     # BitTorrent Files
     torrent)
-        try transmission-show "$path" && { dump | trim; exit 5; } || exit 1;;
+        try transmission-show "$path" && { dump | trim; exit 5; } || exit 2;;
+    # ODT Files
+    odt|ods|odp|sxw)
+        try odt2txt "$path" && { dump | trim; exit 5; } || exit 2;;
+    # Markdown
+    md)
+        try mad "$path" && { dump | trim; exit 5; };;
     # HTML Pages:
     htm|html|xhtml)
         try w3m    -dump "$path" && { dump | trim | fmt -s -w $width; exit 4; }
         try lynx   -dump "$path" && { dump | trim | fmt -s -w $width; exit 4; }
         try elinks -dump "$path" && { dump | trim | fmt -s -w $width; exit 4; }
-        highlight --out-format=ansi --syntax=html "$path" && exit 5 || exit 1;;
-#        ;; # fall back to highlight/cat if the text browsers fail
-    # Bash dotfiles:
+        ;; # fall back to highlight/cat if the text browsers fail
+    # Bash dotfiles
     bash*)
-        highlight --out-format=ansi --syntax=bash "$path" && exit 5 || exit 2;;
-    # Git dotfiles:
+        highlight --out-format=ansi --syntax=bash "$path" && exit 5;;
+    # Git dotfiles
     git*)
-        highlight --out-format=ansi --syntax=ini "$path" && exit 5 || exit 2;;
-    # Markdown:
-    md)
-        mad "$path" && exit 0 || exit 1;;
+        highlight --out-format=ansi --syntax=ini "$path" && exit 5;;
 esac
 
 case "$mimetype" in
     # Syntax highlight for text files:
     text/* | */xml)
-        try highlight --out-format=ansi "$path" && { dump | trim; exit 5; } || exit 2;;
+        if [ "$(tput colors)" -ge 256 ]; then
+            pygmentize_format=terminal256
+            highlight_format=xterm256
+        else
+            pygmentize_format=terminal
+            highlight_format=ansi
+        fi
+        try safepipe highlight --out-format=${highlight_format} "$path" && { dump | trim; exit 5; }
+        try safepipe pygmentize -f ${pygmentize_format} "$path" && { dump | trim; exit 5; }
+        exit 2;;
     # Ascii-previews of images:
     image/*)
-        try cam -W "$width" "$path" && { dump | trim; exit 5; }
-        img2txt --gamma=0.6 --width="$width" "$path" && exit 4 || exit 1;;
-    # Image preview for videos, disabled by default:
-    # video/*)
-    #     ffmpegthumbnailer -i "$path" -o "$cached" -s 0 && exit 6 || exit 1;;
+        # try timg -g${width}x${height} "$path" && { dump; exit 4; }
+        try cam -W "$width" "$path" && { dump; exit 4; }
+        try img2txt --gamma=0.6 --width="$width" "$path" && { dump | trim; exit 4; }
+        exit 2;;
     # Display information about media files:
     video/* | audio/*)
-        try mediainfo "$path" && { dump | trim | sed 's/  \+:/: /;';  exit 5; }
-        try id3info "$path" && { dump | trim; exit 5; }
-        try id3ted -L "$path" && { dump | trim | sed 's/(User defined.\+): //;'; exit 5; }
-        try exiftool -v "$path" && { dump | trim; exit 5; }
-#        try exiftool-5.16 -v "$path" && { dump | trim; exit 5; } || exit 1;;
-#        exiftool "$path" && exit 5
         # Use sed to remove spaces so the output fits into the narrow window
-#        try mediainfo "$path" && { dump | trim | sed 's/  \+:/: /;';  exit 5; } || exit 1;;
-
+        try mediainfo "$path" && { dump | trim | sed 's/  \+:/: /;';  exit 5; }
+        try id3ted -L "$path" && { dump | trim | sed 's/(User defined.\+): //;'; exit 5; }
+        try exiftool "$path" && { dump | trim; exit 5; }
+        try exiftool-5.24 "$path" && { dump | trim; exit 5; }
+        try id3info "$path" && { dump | trim; exit 5; }
+        exit 2;;
 esac
 
 exit 1
