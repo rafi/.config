0d3dd7c6b8834edb4bed72f8e3b96ba081512d06
Rafael Bodill <justrafi@gmail.com>
2017-07-04 21:29:34 +0300

    Update more config files
    - beets: Disable importfeeds and mpdstats

--- mkdirs.sh
+++ mkdirs.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 # Prepare cache directories
-mkdir -p ~/.cache/vim/{backup,bookmarks,swap,plugins,tags,undo}
-mkdir -p ~/.cache/{aria2,beets,mpd,mpdscribble,mutt,pacaur,rtorrent,subtitles,mux,z}
+mkdir -p ~/.cache/vim/{backup,session,swap,tags,undo}
+mkdir -p ~/.cache/{aria2,beets,mpd,mpdscribble,mutt,pacaur,rtorrent,subtitles,z}
 mkdir -p ~/.cache/ncmpcpp/lyrics
 mkdir -p ~/.cache/node/{npm,gyp}
 
 # Prepare user local shared directories
 mkdir -p ~/.local/bin
-mkdir -p ~/.local/share/{composer,fonts,vagrant,mutt,newsbeuter,virtualbox}
-mkdir -p ~/.local/lib/{node,python2.7,ruby}
-mkdir -p ~/.local/lib/python2.7/virtualenvs
+mkdir -p ~/.local/share/{cargo,composer,fonts,vagrant,mutt,newsbeuter,virtualbox}
+mkdir -p ~/.local/share/python/{envs,utils}
