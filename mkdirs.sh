59d40b14bc8dc6146289c70ce9412385240dcb5c
Rafael Bodill <justrafi@gmail.com>
2020-12-11 13:11:43 +0200

    Update home directory scheme

--- mkdirs.sh
+++ mkdirs.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 # Prepare cache directories
-mkdir -p ~/.cache/vim/{backup,complete,session,swap,tags,undo}
+mkdir -p ~/.cache/vim/{backup,session,swap,tags,undo}
 mkdir -p ~/.cache/{aria2,beets,mpd,mpdscribble,mutt,pacaur,rtorrent,subtitles,z}
 mkdir -p ~/.cache/ncmpcpp/lyrics
-mkdir -p ~/.cache/node/{npm,gyp}
 
 # Prepare user local shared directories
 mkdir -p ~/.local/bin
-mkdir -p ~/.local/share/{cargo,composer,fonts,vagrant,tig,mutt,newsbeuter,virtualbox}
+mkdir -p ~/.local/share/{cargo,composer,fonts,lf,vagrant,tig,mutt,newsbeuter,virtualbox}
 mkdir -p ~/.local/share/python/envs
